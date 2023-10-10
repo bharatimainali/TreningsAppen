9fbfd8e96c0531daaaf583849502858aa4ec8cf8
Bharati Mainali <bharatimainali@gmail.com>
2023-10-10 15:54:45 +0200

    change to older file which was created by marius

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,6 @@ buildscript {
 
 
 dependencies {
-    implementation ("androidx.fragment:fragment-ktx:1.3.6")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
