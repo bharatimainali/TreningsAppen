e7334204ed69ef882e0c91a8dbe2f38febdae8ad
Marius Begby <mariusbegby@gmail.com>
2023-10-11 19:48:05 +0200

    Load the exercises and display as cards from the DataSource

--- app/src/main/java/hiof/gruppe15/treningsappen/MainActivity.kt
+++ app/src/main/java/hiof/gruppe15/treningsappen/MainActivity.kt
@@ -3,15 +3,33 @@ package hiof.gruppe15.treningsappen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.example.treningsappen.R
 import hiof.gruppe15.treningsappen.ui.theme.TreningsAppenTheme
-// as i understand that Marius will implement so i didn't touch here.
+import hiof.gruppe15.treningsappen.data.Datasource
+import hiof.gruppe15.treningsappen.model.Exercise
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("World")
+                    WorkoutApp()
                 }
             }
         }
@@ -31,17 +49,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun WorkoutApp() {
+    val context = LocalContext.current
+    val exercises = Datasource().loadExercisesFromJson(context)
+
+    ExerciseList(exercises = exercises)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TreningsAppenTheme {
-        Greeting("Android")
+fun ExerciseList(exercises: List<Exercise>, modifier: Modifier = Modifier) {
+    LazyColumn(userScrollEnabled = true,
+        modifier = modifier) {
+        items(exercises.size) { index ->
+            ExerciseCard(exercise = exercises[index])
+        }
+    }
+}
+
+@Composable
+fun ExerciseCard(exercise: Exercise, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(
+                text = exercise.name,
+                style = MaterialTheme.typography.headlineMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Text(text = exercise.description)
+                }
+            }
+
+        }
     }
 }
